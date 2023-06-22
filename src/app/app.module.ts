dc25e18f3a0429ea3bbcc334a6356ec5cb49f70d
YoyoGZ <117695561+YoyoGZ@users.noreply.github.com>
2023-06-22 17:47:45 -0300

    First Commit Proyecto3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
+import { MyPComponent } from './components/my-p/my-p.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainWrapperComponent,
+    MyPComponent
   ],
   imports: [
     BrowserModule
